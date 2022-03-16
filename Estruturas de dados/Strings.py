fdc824648289ae9dcfaf59901a356e44702c2de2
dev-Pedrod <pedroolivxira@gmail.com>
2022-03-16 09:34:20 -0300

    Strings ou sequencia de caracteres

---
+++ Estruturas de dados/Strings.py
@@ -0,0 +1,33 @@
+texto = "Aprendendo Python na disciplina de linguagem de programação."
+
+print(f"Tamanho do texto = {len(texto)}")
+print(f"Python in texto = {'Python' in texto}")
+print(f"Quantidade de y no texto = {texto.count('y')}")
+print(f"As 5 primeiras letras são: {texto[0:6]}")
+
+print(texto.upper())
+print(texto.replace("i", 'XX'))
+
+print(f"texto = {texto}")
+print(f"Tamanho do texto = {len(texto)}\n")
+
+palavras = texto.split()
+print(f"palavras = {palavras}")
+print(f"Tamanho de palavras = {len(palavras)}")
+
+texto = """Operadores de String
+Python oferece operadores para processar texto (ou seja, valores de string).
+Assim como os números, as strings podem ser comparadas usando operadores de comparação:
+==, !=, <, > e assim por diante.
+O operador ==, por exemplo, retorna True se as strings nos dois lados do operador tiverem o mesmo valor (Perkovic, p. 23, 2016).
+"""
+
+print(f"Tamanho do texto = {len(texto)}")
+texto = texto.lower()
+texto = texto.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace("\n", " ")
+lista_palavras = texto.split()
+print(f"Tamanho da lista de palavras = {len(lista_palavras)}")
+
+total = lista_palavras.count("string") + lista_palavras.count("strings")
+
+print(f"Quantidade de vezes que string ou strings aparecem = {total}")
